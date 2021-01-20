3091a523700d8a83aa1d6436f90a103f7a946846
Averill Cate, Jr <acatejr@gmail.com>
2021-01-20 07:48:33 -0700

    Svelte for beginners tutorial finished video 7

--- svelte/tutorial.for.beginners/src/App.svelte
+++ svelte/tutorial.for.beginners/src/App.svelte
@@ -1,35 +1,15 @@
 <script>
-	/*
-	let firstName = 'Jimmy'
-	let lastName = 'Hendrix'
-	let beltColour = 'brown'
-	*/
+	// Test data
 	let people = [
 		{ name: 'yoshi', beltColour: 'black', age: 25, id: 1},
 		{ name: 'mario', beltColour: 'orange', age: 45, id: 2},
 		{ name: 'luigi', beltColour: 'brown', age: 35, id: 3},
 	]
 
-	// // Reactive value
-	// $: fullName = `${firstName} ${lastName}`
-
-    // // Reactive statement
-	// $: console.log(beltColour)
-
-	// // Reactive statement
-	// $: {
-	// 	console.log(firstName)
-	// 	console.log(lastName)
-	// 	console.log(fullName)
-	// }
-
-	// const handleClick = () => {
-	// 	beltColour = 'green'
-	// }
-
-	// const handleInput = (e) => {
-	// 	beltColour = e.target.value
-	// }
+	const handleClick = (id) => {
+		// delete the person from people
+		people = people.filter((person) => person.id !== id)
+	}
 
 </script>
 
@@ -38,18 +18,11 @@
 		<div>
 			<h4>{person.name}</h4>
 			<p>{person.age} years old, {person.beltColour} belt.</p>
+			<button on:click={() => handleClick(person.id)}>Delete</button>
 		</div>
 	{:else}
 		<p>There are no people to show...</p>
 	{/each}
-
-	<!--
-	<p>{fullName} {beltColour} belt</p>
-	<input type='text' bind:value={firstName}>
-	<input type='text' bind:value="{lastName}">
-	<input type='text' bind:value={beltColour}>
-	<button on:click={handleClick}>Update Belt Colour</button>	
-	-->
 </main>
 
 <style>
