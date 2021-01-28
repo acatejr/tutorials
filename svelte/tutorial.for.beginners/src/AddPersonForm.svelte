5761e88e4607d1a6a785de3fd8effbae86e67bfc
Averill Cate, Jr <acatejr@gmail.com>
2021-01-28 15:06:53 -0700

    Finished svelte beginner video 16

--- svelte/tutorial.for.beginners/src/AddPersonForm.svelte
+++ svelte/tutorial.for.beginners/src/AddPersonForm.svelte
@@ -2,17 +2,30 @@
     let name
     let beltColour
     let age
+    let skills = []
 
     const handleSubmit = () => {
-        console.log(name, beltColour, age)
+        console.log(name, beltColour, age, skills)
     }
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="Name" bind:value={name}>
-    <input type="text" placeholder="Belt Colour" bind:value={beltColour}>
+    <!-- <input type="text" placeholder="Belt Colour" bind:value={beltColour}> -->
     <input type="number" placeholder="Age" bind:value={age}>
-	<button>Add Person</button>
+    <label>Skils:</label>
+    <input type="checkbox" bind:group={skills} value='fighting'>fighting<br/>
+    <input type="checkbox" bind:group={skills} value='sneaking'>sneaking<br/>
+    <input type="checkbox" bind:group={skills} value='running'>running<br/>
+    <button>Add Person</button>
+    
+    <label>Belt Colour:</label>
+    <select bind:value={beltColour}>
+        <option value="black">black</option>
+        <option value="orange">orange</option>
+        <option value="brown">brown</option>
+        <option value="white">white</option>
+    </select>
 </form>
 
 <style></style>
